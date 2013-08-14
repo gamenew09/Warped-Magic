b456d87f3e7765d5ad417fb02b97a236563d03ab
gamenew09 <gamenew09@gmail.com>
2013-08-14 13:01:39 -0500

    Big commit! Actual mod files. :D

---
+++ common/com/gamenew09/warpedmagic/WarpedMagicMod.java
@@ -0,0 +1,29 @@
+package com.gamenew09.warpedmagic;
+
+import com.gamenew09.warpedmagic.lib.Reference;
+
+import cpw.mods.fml.common.*;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.*;
+import cpw.mods.fml.common.network.*;
+
+@Mod(modid=Reference.MOD_ID, name=Reference.MOD_NAME, version=Reference.MOD_VERSION)
+@NetworkMod(clientSideRequired=true, serverSideRequired=false)
+public class WarpedMagicMod {
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event){
+		
+	}
+	
+	@EventHandler
+	public void initMod(FMLInitializationEvent event){
+		
+	}
+	
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event){
+		
+	}
+	
+}
---
+++ common/com/gamenew09/warpedmagic/lib/Reference.java
@@ -0,0 +1,9 @@
+package com.gamenew09.warpedmagic.lib;
+
+public class Reference {
+	
+	public static final String MOD_ID = "WMM";
+	public static final String MOD_NAME = "Warped Magic";
+	public static final String MOD_VERSION = "0.0.1";
+	
+}
