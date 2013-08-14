a4ad58f5db3b953474d5a502a346223bd0b6f3d1
gamenew09 <gamenew09@gmail.com>
2013-08-14 16:31:20 -0500

    Stuff more stuff.

--- common/com/gamenew09/warpedmagic/WarpedMagicMod.java
+++ common/com/gamenew09/warpedmagic/WarpedMagicMod.java
@@ -1,5 +1,9 @@
 package com.gamenew09.warpedmagic;
 
+import java.util.logging.Logger;
+
+import buildcraft.api.core.BuildCraftAPI;
+
 import com.gamenew09.warpedmagic.lib.Reference;
 
 import cpw.mods.fml.common.*;
@@ -10,6 +14,17 @@ import cpw.mods.fml.common.network.*;
 @Mod(modid=Reference.MOD_ID, name=Reference.MOD_NAME, version=Reference.MOD_VERSION)
 @NetworkMod(clientSideRequired=true, serverSideRequired=false)
 public class WarpedMagicMod {
+	
+	public static final Logger logger = Logger.getLogger(WarpedMagicMod.class.getSimpleName());
+	
+	public static boolean isBuildcraftInstalled(){
+		try {
+			Class.forName("buildcraft.api.core.BuildCraftAPI");
+			return true;
+		} catch (ClassNotFoundException e) {
+			return false;
+		}
+	}
 	
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event){
